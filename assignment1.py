da1c0ec7c5d45c9862845df3248acb2cdf60685a
Taytatat <157549258+Taytatat@users.noreply.github.com>
2025-02-17 23:24:42 -0600

    Add files via upload
    pleaseeeeeee

--- assignment1.py
+++ assignment1.py
@@ -28,17 +28,23 @@ model = Prophet(changepoint_prior_scale=0.5) #chaangepoint prior scale needs to 
 modelFit = model.fit(dataFilter)  
 
 # Create an empty dataframe with dates for future periods
-future = modelFit.make_future_dataframe(periods=744, freq ='H') #make the number of periods 744 for the 744 hours in January of 2019 (the following year), make the frequency = 'H' so that the predictions are made in hours
+future = modelFit.make_future_dataframe(periods=744, freq ='H') #make the number of periods 745 for the 745 (this will end up giving 744 which is what we want, idk why) hours in January of 2019 (the following year), make the frequency = 'H' so that the predictions are made in hours
 # Predict will fill in  empty dataframe wtih forecasts of `y` for the future periods
 pred_full = modelFit.predict(future)
 
 pred_full.head() #to get a quick pic of the forecasts created 
 
-#this will give us back out 744 periods of forcast at the end of the data frame, this will create a list of floats
-pred = pred_full['yhat'][-744:]
+#this will give us back out 745 (this will end up giving 744 which is what we want, idk why) periods of forcast at the end of the data frame, this will create a list of floats
+pred_float = pred_full['yhat'][-744:]
 
 #also now need to covert numbersin pred from floats to integers. Will give a list of vlaues back
 #pred = [int(x) for x in pred_float] 
+
+#np.array(pred).tolist
+
+#was able to find that for some reason 7492 in pred list was non numeric and was instead <class 'numpy.int64'> for some reason, so doing this code to hopefully turn that into a numeric element too
+pred = [int(x) if isinstance(x, (str, np.int64, np.int32)) and (str(x) == '7492' or str(x) == '7483')else x for x in pred_float]
+
 
 fig1 = modelFit.plot(pred_full)
 fig1.show()
@@ -46,3 +52,33 @@ fig1.show()
 fig2 = modelFit.plot_components(pred_full)
 fig2.show()
 
+
+#this is the code that got 8/9 but for some reason i cant get the third test. When i do the checks
+#i keep getting back that the check numbers isnt working and its seems to be because the list has a non numeric element
+#but all of my attempts to fix the numpy.int32 have been fruitless idk whats goin onnnnnn
+def checkNumbers(series):
+    for i in series:
+        if not isinstance(i, (float, int)):
+          print("Non-numeric element found:", i, type(i)) #helps me to find the non-numeric stuff poping up in my check numbers list
+          return False
+    return True
+
+
+#class testCases(unittest.TestCase):
+   # def testValidPred(self):
+        #self.assertTrue((len(np.squeeze(pred))==744 and checkNumbers(np.squeeze(pred))), "Make sure your prediction consists of integers\nor floating point numbers, and is a list or array of 744\nfuture predictions!")
+
+print(len(np.squeeze(pred)))   
+
+#print(np.squeeze(pred))
+
+checkNumbers(np.squeeze(pred))
+#pred_list = np.array(pred)
+
+#checkNumbers(np.squeeze(pred_list))
+
+#print(type(pred))
+
+#index = pred.index('7483')
+
+#print(index)
